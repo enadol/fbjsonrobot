1b7e51232538a902083e8e1e3dba537b2c6b94e9
Enrique Lopez Magallon <enadol@yahoo.com>
2017-08-03 18:35:28 +0200

    Update fbjsonbis.py

--- fbjsonbis.py
+++ fbjsonbis.py
@@ -3,7 +3,7 @@ import re
 #to open the two files of the season
 
 #url=('https://raw.githubusercontent.com/openfootball/de-deutschland/master/2016-17/1-bundesliga-i.txt')
-fh=open('bundesliga2016.txt')
+fh=open('bundesliga2017.txt')
 clubcodes={"Eintracht Frankfurt": "FFM","1899 Hoffenheim": "HOF","Bayern Muenchen": "FCB","VfL Wolfsburg": "WOB","Borussia Dortmund": "BVB","Hamburger SV": "HSV","FC Augsburg": "FCA","RB Leipzig": "RBL","SC Freiburg":"SCF","Hertha BSC":"BSC","Werder Bremen": "BRE","Bor. Moenchengladbach":"BMG", "Bayer Leverkusen":"B04","1. FC Koeln":"KOE","FC Ingolstadt 04":"FCI","SV Darmstadt 98":"D98","FC Schalke 04":"S04","1. FSV Mainz 05":"M05"}
 clubkeys={"Eintracht Frankfurt":"frankfurt", "1899 Hoffenheim":"hoffenheim","Bayern Muenchen": "bayern","VfL Wolfsburg":"wolfsburg","Borussia Dortmund":"dortmund","Hamburger SV":"hsv","FC Augsburg":"augsburg","RB Leipzig": "leipzig","SC Freiburg":"freiburg","Hertha BSC":"herthabsc","Werder Bremen":"bremen","Bor. Moenchengladbach": "mgladbach","Bayer Leverkusen":"leverkusen","1. FC Koeln":"koeln","FC Ingolstadt 04":"ingolstadt","SV Darmstadt 98":"darmstadt","FC Schalke 04":"schalke","1. FSV Mainz 05":"mainz"}
 lsthome=[]
