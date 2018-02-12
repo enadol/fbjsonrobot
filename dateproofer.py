a361725ced9928f774317cfdebd422d7b9b5f54a
Enrique Lopez Magallon <enadol@yahoo.com>
2018-02-12 12:30:46 +0100

    Update dateproofer.py

--- dateproofer.py
+++ dateproofer.py
@@ -6,7 +6,7 @@ diadef=[]
 
 #for special matchdays with three matches on sunday, beginning season 2017/2018.
 #Replace/check items in array every new season
-specialMD=frozenset([4, 7, 9, 15])
+specialMD=frozenset([4, 7, 9, 15, 27])
 conlunes=frozenset([23,26])
 
 #print lstnuevafecha
