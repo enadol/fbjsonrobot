14fc3a0de343830d4f8c5b1019deca75b8b49df1
Enrique Lopez Magallon <enadol@yahoo.com>
2019-12-15 21:43:18 +0100

    Add files via upload

--- launch.py
+++ launch.py
@@ -1,13 +1,29 @@
 #!python3
+try:
 
-import urllib.request
-import prelaunch
-import createtext
-import fbjson
-import jsonbuilder
-import daterobot
-import dateproofer
-import jsonrobot
-import jsonformat
+	import subprocess
+	import urllib.request
+	import prelaunch #NEW
+	import createtext #NEW
+	import fbjson #NEW
+	import jsonbuilder
+	import daterobot
+	import dateproofer
+	import jsonrobot
+	import jsonformat
 
-print("Launching robot...")
+	print("Launching robot...")
+
+	
+except IndexError:
+	print("La jornada no ha finalizado. Matchday not finished.")
+
+except OSError as err:
+    print("OS error: {0}".format(err))
+
+except ValueError:
+    print("Could not convert data to an integer.")
+    
+except:
+    print("Unexpected error:", sys.exc_info()[0])
+    raise
